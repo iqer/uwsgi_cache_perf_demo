c32bf19b3ba03ac6efd92e7e3d68d64ddeab5bc5
ArthurChen <771562385@qq.com>
2021-03-25 16:30:35 +0800

    Delete demo.sql

--- demo.sql
+++
@@ -1,4 +0,0 @@
-USE planck_db;
-ALTER table plk_order ADD INDEX `idx_create_time`(create_time);
-ALTER table plk_order_group_map ADD INDEX `idx_create_time`(create_time);
-ALTER table dj_pool_order ADD INDEX `idx_create_time`(create_time);
